9255f7a43608ea4efcc4baeb49e6ca3d8733b40c
mjkim0203 <mjkim020203@ewhain.net>
2025-07-21 06:50:19 +0900

    Update paho-mqtt-min.js

--- js/paho-mqtt-min.js
+++ js/paho-mqtt-min.js
@@ -1,18 +1,13 @@
-
-/*!
- * Copyright (c) 2013, 2018 IBM Corp.
+/*
+ * Licensed Materials - Property of IBM
+ * (c) Copyright IBM Corp. 2011, 2013
+ * All Rights Reserved
  *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * Eclipse Paho JavaScript Client
+ * https://github.com/eclipse/paho.mqtt.javascript
  *
- * Contributors:
- *    James Sutton - Initial Contribution
+ * @license See https://www.eclipse.org/legal/epl-v10.html
  */
-
-/* Paho MQTT JavaScript client library v1.1.0 - mqttws31.min.js */
-
-/* NOTE: Truncated for demo - in actual use, full file should be loaded from CDN or official repo */
-var Paho={MQTT:{}};Paho.MQTT.Client=function(server,port,path,clientId){this.server=server;this.port=port;this.path=path;this.clientId=clientId;console.log("Mock Paho.MQTT.Client created")};
-Paho.MQTT.Message=function(payload){this.payloadString=payload;};
+var Paho=Paho||{};
+Paho.MQTT=Paho.MQTT||function(){function d(b,a,c){this.name=b;this.message=a;this.stack=c}function w(b){return null==b||"string"===typeof b}function B(b){return"number"===typeof b}function p(b){return"boolean"===typeof b}function x(b){return b instanceof ArrayBuffer}function y(b){return"function"===typeof b}function z(b){return void 0!==b&&null!==b}function D(b){return"[object Array]"===Object.prototype.toString.call(b)}function C(b){return"object"===typeof b&&!D(b)}function q(b,a){if(null==b)throw Error(a+" is null.");
+if(void 0===b)throw Error(a+" is undefined.");}function G(b,a,c,e){this.type=b;this.clientId=a;this.errorMessage=c;this.errorCode=e}function H(b,a,c,e){this.type=b;this.invocationContext=a;this.errorMessage=c;this.errorCode=e}function I(b,a,c){this.type=b;this.destinationName=a;this.payloadString=c}function J(b,a){return function(){if(1!==b._traceBuffer.length){if(b._traceBuffer.length>=b.traceBufferSize)b._traceBuffer.shift();b._traceBuffer.push(a.apply(b,arguments))}}}
